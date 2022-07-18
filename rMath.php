e292a137a1878a42dea56703c0fd5dc17329783d
m-maekakuchi <m.marikoromo@gmail.com>
2022-07-18 21:41:15 +0900

    $listを$arrayに変更

--- rMath.php
+++ rMath.php
@@ -9,33 +9,33 @@ class rMath {
   }
 
   //データの合計値を求める
-  function sum_data($list) {
+  function sum_data($array) {
     $sumData = 0;
-    foreach ($list as $value) {
+    foreach ($array as $value) {
         $sumData += $value;
     }
     return $sumData;
   }
 
   // データの平均値を求める
-  function ave_data($list) {
-    $average = $this->sum_data($list) / $this->dataNum;
+  function ave_data($array) {
+    $average = $this->sum_data($array) / $this->dataNum;
     return $average;
   }
 
   // 偏差
   //個々の数値 - データの平均値
-  function calc_deviation($value, $list) {
-    $diviation = $value - $this->ave_data($list);
+  function calc_deviation($value, $array) {
+    $diviation = $value - $this->ave_data($array);
     return $diviation;
   }
 
   //分散
   //偏差の2乗を全て足した値を、データ数で割る
-  function calc_variance($list) {
+  function calc_variance($array) {
     $sum_deviation = 0;
-    foreach ($list as $value) {
-        $sum_deviation += pow($this->calc_deviation($value, $list), 2);
+    foreach ($array as $value) {
+        $sum_deviation += pow($this->calc_deviation($value, $array), 2);
     }
     $variance = $sum_deviation / $this->dataNum;
     return $variance;
@@ -43,10 +43,10 @@ class rMath {
 
   // 共分散
   //2つの変数の偏差の積の平均値
-  function calc_covariance($xlist, $ylist) {
+  function calc_covariance($xArray, $yArray) {
     $mul_deviation = 0;
     for ($i = 0; $i < $this->dataNum; $i++) {
-      $mul_deviation += $this->calc_deviation($xlist[$i], $xlist) * $this->calc_deviation($ylist[$i], $ylist);
+      $mul_deviation += $this->calc_deviation($xArray[$i], $xArray) * $this->calc_deviation($yArray[$i], $yArray);
     }
     $covariance = $mul_deviation / $this->dataNum;
     return $covariance;
@@ -54,16 +54,16 @@ class rMath {
 
   //標準偏差(SD)
   //分散の正の平方根
-  function calc_standardDeviation($list) {
-    $standard_deviation = $this->calc_variance($list);
+  function calc_standardDeviation($array) {
+    $standard_deviation = $this->calc_variance($array);
     return sqrt($standard_deviation);
   }
 
   //相関係数ｒ
-  function calc_r($xlist, $ylist) {
-    $x_SD = $this->calc_standardDeviation($xlist);
-    $y_SD = $this->calc_standardDeviation($ylist);
-    $r = $this->calc_covariance($xlist, $ylist) / ($x_SD * $y_SD);
+  function calc_r($xArray, $yArray) {
+    $x_SD = $this->calc_standardDeviation($xArray);
+    $y_SD = $this->calc_standardDeviation($yArray);
+    $r = $this->calc_covariance($xArray, $yArray) / ($x_SD * $y_SD);
     return $r;
   }
 }
